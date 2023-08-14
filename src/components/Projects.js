6c3d7ed3adac5a5f8ea37eeb5837f0a364a7a53c
KaczmarekBartosz <bartosz.kaczmarek@icloud.com>
2023-08-14 19:08:12 +0200

    Update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,16 +3,58 @@ import "./Projects.css";
 import FITX from "../images/FITXProject.PNG";
 import FOTOCAMPER from "../images/FOTOCAMPERProject.PNG";
 import SOPRANO from "../images/SOPRANOProject.PNG";
+import { TypeAnimation } from "react-type-animation";
 
 export const Projects = () => {
   return (
     <div>
       <section id="projects">
-        <h2>Projects I'm proud of</h2>
+        <h2>
+          {/* <TypeAnimation
+            sequence={[
+              "Projects I'm proud of",
+              1000,
+              "Projects I've worked On",
+              1000,
+              "My project achievements",
+              1000,
+              "Creative realizations",
+              1000,
+              "My projects in action",
+              1000,
+            ]}
+            speed={50}
+            repeat={Infinity}
+          /> */}
+          <TypeAnimation
+            sequence={[
+              "Projects I'm proud of",
+              1000,
+              "Welcome to my creative world",
+              1000,
+              "Exploring innovative creations",
+              1000,
+              "Showcasing passion-driven ventures",
+              1000,
+              "Designing with heart and soul",
+              1000,
+              "Bringing ideas to digital life",
+              1000,
+            ]}
+            speed={40}
+            repeat={Infinity}
+          />
+          <h4>
+            Discover a collection of projects that define my journey as a
+            creator.
+          </h4>
+        </h2>
         <article>
           <div class="text">
             <h4>Latest Project</h4>
-            <h3>FIT-X Catering Dietetyczny</h3>
+            <h3>
+              <a href="https://www.fit-x.pl">FIT-X Catering Dietetyczny</a>
+            </h3>
             <p class="blackbox">
               I created a modern website for FIT-X diet catering based on the
               WordPress platform. The project involved designing an attractive
@@ -22,8 +64,7 @@ export const Projects = () => {
               responsive, adapting to various devices, enabling convenient use
               on smartphones and tablets. Additionally, the project included SEO
               optimization to improve the website's visibility and ranking in
-              search results. You can check the whole page here:
-              <a href="https://www.fit-x.pl">www.fit-x.pl</a>.
+              search results.
             </p>
             <h4>Technologies used include:</h4>
             <ul>
@@ -41,7 +82,11 @@ export const Projects = () => {
         <article>
           <div class="text">
             <h4>Commercial Project</h4>
-            <h3>FOTOCAMPER - Mobilna fotobudka</h3>
+            <h3>
+              <a href="https://www.fotocamper.pl">
+                FOTOCAMPER - Mobilna fotobudka
+              </a>
+            </h3>
             <p class="blackbox">
               As part of my commercial projects, I created the website
               www.fotocamper.pl, which promotes the offer of renting a mobile
@@ -54,8 +99,7 @@ export const Projects = () => {
               enabling convenient use on both computers and mobile devices. The
               design was executed in a BOHO style. The project also included SEO
               optimization to increase the website's visibility in search
-              results. You can check the whole page here:
-              <a href="https://www.fotocamper.pl">www.fotocamper.pl</a>.
+              results.
             </p>
             <h4>Technologies used include:</h4>
             <ul>
@@ -73,7 +117,11 @@ export const Projects = () => {
         <article>
           <div class="text">
             <h4>Commercial Project</h4>
-            <h3>SOPRANO Baranów - Pizzeria & PUB</h3>
+            <h3>
+              <a href="https://www.soprano-baranow.pl">
+                SOPRANO Baranów - Pizza & Pub
+              </a>
+            </h3>
             <p class="blackbox">
               As part of my commercial projects, I completely redesigned the
               website of SOPRANO - Pizzeria and PUB, which promotes the
